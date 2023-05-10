e68514234d2fb19aaa552207fd5f34edfd0d81a1
Changwan Jun <wan2land@gmail.com>
2023-05-11 01:21:06 +0900

    chore: remove deps

--- cli.ts
+++ cli.ts
@@ -1,6 +1,5 @@
 // deno-lint-ignore-file no-explicit-any
 
-import { parse } from "./deps.ts";
 import { Hasher } from "./hasher.ts";
 
 const isDeno = typeof (globalThis as any).Deno !== "undefined";
@@ -10,10 +9,12 @@ const cmd = isDeno
   : "npx inthash";
 
 const args = parse(
-  isDeno ? (globalThis as any).Deno.args : (globalThis as any).process.argv,
+  isDeno
+    ? (globalThis as any).Deno.args
+    : (globalThis as any).process.argv.slice(2),
 );
 
-const options = Hasher.generate(+(args.b ?? args.bit ?? args.bits ?? "53"));
+const options = Hasher.generate(args.b ?? args.bit ?? args.bits ?? 53);
 
 console.log(JSON.stringify(options, null, "  "));
 console.error(`
@@ -21,3 +22,35 @@ console.error(`
 $ ${cmd} | pbcopy
 
 then paste to your code! :-) good luck.`);
+
+type Args = {
+  b?: number;
+  bit?: number;
+  bits?: number;
+};
+
+function parse(args: string[]): Args {
+  const argv: Args = {};
+
+  for (let i = 0; i < args.length; i++) {
+    const arg = args[i];
+    let match;
+    if ((match = arg.match(/^--(b|bit|bits)=(\d+)/))) {
+      const [, key, value] = match;
+      argv[key as "b" | "bit" | "bits"] = +value;
+    } else if ((match = arg.match(/^--(b|bit|bits)$/))) {
+      const [, key] = match;
+      const next = args[i + 1];
+      if (
+        next &&
+        /\d+/.test(next)
+      ) {
+        argv[key as "b" | "bit" | "bits"] = +next;
+        i++;
+      }
+    } else if ((match = arg.match(/^-b(\d+)/))) {
+      argv.b = +match[1];
+    }
+  }
+  return argv;
+}
--- deps.ts
+++
@@ -1,1 +0,0 @@
-export { parse } from "https://deno.land/std@0.167.0/flags/mod.ts";
